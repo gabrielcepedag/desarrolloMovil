88b8c4af3a4bde87c2dfe7537ac5d5dd6d8629d7
gabrielcg01 <gcepeda@orkapi.com>
2023-09-29 00:16:20 -0500

    actividad con botones para ambas vistas

---
+++ Tarea2/app/src/main/java/com/example/tarea2/ListOrRecyclerView.java
@@ -0,0 +1,37 @@
+package com.example.tarea2;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListOrRecyclerView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_or_recycler_view);
+
+        Button listViewButton = findViewById(R.id.listViewButton);
+        Button recyclerViewButton = findViewById(R.id.recyclerViewButton);
+
+        listViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inicia MainActivity con ListView
+                Intent intent = new Intent(ListOrRecyclerView.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        recyclerViewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inicia MainActivity con RecyclerView
+                Intent intent = new Intent(ListOrRecyclerView.this, RecyclerViewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Tarea2/app/src/main/java/com/example/tarea2/RecyclerViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.tarea2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecyclerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view);
+    }
+}
