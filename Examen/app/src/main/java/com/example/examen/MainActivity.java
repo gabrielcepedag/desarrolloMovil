2450dd13c2bdffa38c7b54f10520d0165b3b8ea0
gabrielcg01 <gcepeda@orkapi.com>
2023-10-09 18:19:14 -0500

    creando adapter

--- Examen/app/src/main/java/com/example/examen/MainActivity.java
+++ Examen/app/src/main/java/com/example/examen/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.examen.adapter.ProductoAdapter;
+import com.example.examen.api.APIClient;
+import com.example.examen.api.ApiProducto;
+import com.example.examen.dto.Producto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv_products);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ProductoAdapter productoAdapter =  new ProductoAdapter(new ProductoAdapter.ProductoDiff());
+        recyclerView.setAdapter(productoAdapter);
+
+//        List<Producto> prueba = new ArrayList<>();
+//        prueba.add(new Producto(1, "TITULO DE PRUEBA", "HOLA SIY UNA PAKJSAJS ALSJALSK LKASMAS AKSMASMLAS MAKLSAKLAKNSAkdnsfsdkf", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//        prueba.add(new Producto(2, "TITULO DE PRUEBA 2", "22222222HOLA SIY UNA PAKJSAJS ALSJALSK LKASMA", new ArrayList<>()));
+//
+//        productoAdapter.submitList(prueba);
+
+        ApiProducto api = APIClient.getClient().create(ApiProducto.class);
     }
 }
---
+++ Examen/app/src/main/java/com/example/examen/adapter/ProductoAdapter.java
@@ -0,0 +1,78 @@
+package com.example.examen.adapter;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.examen.R;
+import com.example.examen.dto.Producto;
+import org.jetbrains.annotations.NotNull;
+
+public class ProductoAdapter extends ListAdapter<Producto, ProductoAdapter.ProductoViewHolder> {
+
+
+
+    public ProductoAdapter(@NonNull @NotNull DiffUtil.ItemCallback<Producto> diffCallback) {
+        super(diffCallback);
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public ProductoViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return ProductoViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ProductoViewHolder holder, int position) {
+        Producto current = getItem(position);
+        holder.bind(current);
+
+        Log.w("ESTOY EN", "onBindViewHolder");
+    }
+
+    public static class ProductoDiff extends DiffUtil.ItemCallback<Producto> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull @NotNull Producto oldItem, @NonNull @NotNull Producto newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull @NotNull Producto oldItem, @NonNull @NotNull Producto newItem) {
+            return oldItem.getTitle().equals(newItem.getTitle());
+        }
+    }
+
+    static class ProductoViewHolder extends RecyclerView.ViewHolder {
+//        private final ImageView imageView;
+        private final TextView textViewTitulo;
+        private final TextView textViewDescripcion;
+
+        public ProductoViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+//            this.imageView = itemView.findViewById(R.id.iv_imagen);
+            this.textViewTitulo = itemView.findViewById(R.id.tv_titulo);
+            this.textViewDescripcion = itemView.findViewById(R.id.tv_descripcion);
+        }
+
+        public void bind(Producto producto){
+//            Glide.with(TaskViewHolder.this).load(producto.getImages())
+            textViewTitulo.setText(producto.getTitle());
+            textViewDescripcion.setText(producto.getDescription());
+        }
+
+        static ProductoViewHolder create(ViewGroup parent){
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.activity_item_product, parent, false);
+
+            return new ProductoViewHolder(view);
+        }
+
+    }
+}
